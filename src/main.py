79dacede83304ef46fe7a6d24fd7aa7592ae0472
jymbocala <jym.bocala@gmail.com>
2023-10-23 22:04:02 +1100

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,46 +1,74 @@
 # IMPORTS
 from wonderwords import RandomWord
 import wordle
+
 # MAIN
-# Introduction
-print('Welcome to Wordle! \n👋 Hello there! I\'m your Wordle companion. Before we dive into the word-guessing fun, may I know your name? Please type it below, and we\'ll get started:')
-name = input(str('Enter your name:  '))
-print(f'Great! Hi, {name}! It\'s nice to meet you. Now, let me explain how Wordle works. \n🧩 Wordle is a word-guessing game where you\'ll need to guess a secret five-letter word. You have six attempts to guess the word correctly. After each guess, I\'ll provide feedback to help you narrow down the possibilities. Each letter in the word will be color-coded to let you know if it\'s in the word, and if it\'s in the right position. Your goal is to crack the code as quickly as possible! \nTo make a guess, simply type a five-letter word, and I\'ll let you know how you\'re doing. \nAre you ready to start playing and put your word skills to the test? \n')
+if __name__ == "__main__":
+  # Introduction
+  print('Welcome to Wordle! \n👋 Hello there! I\'m your Wordle companion. Before we dive into the word-guessing fun, may I know your name? Please type it below, and we\'ll get started:')
+  name = input(str('Enter your name:  '))
+  print(f'Great! Hi, {name}! It\'s nice to meet you. Now, let me explain how Wordle works. \n🧩 Wordle is a word-guessing game where you\'ll need to guess a secret five-letter word. You have six attempts to guess the word correctly. After each guess, I\'ll provide feedback to help you narrow down the possibilities. Each letter in the word will be color-coded to let you know if it\'s in the word, and if it\'s in the right position. Your goal is to crack the code as quickly as possible! \nTo make a guess, simply type a five-letter word, and I\'ll let you know how you\'re doing. \nAre you ready to start playing and put your word skills to the test? \n')
 
-
-def start_new_game():
-  print('START')
   wordle_word = RandomWord().word(word_min_length=5, word_max_length=5)
-  print(f'The word was {wordle_word}')
-  wordle.GameSession(wordle_word)
+  print(f'Wordle_word: {wordle_word}')
+  
+  game = wordle.GameSession(wordle_word)
+  
 
-def user_help():
-  print('Help')
+  def user_help():
+    pass
+    # print some helpful information about how the game works
 
-def ask_if_player_is_ready():
-  q = input(
-    str('Type \'YES\' to begin or \'HELP\' if you\'d like more instructions: '))
-  if q == 'YES' or q == 'yes':
-    print('Starting new game...')
-    start_new_game()
-  elif q == 'HELP' or q == 'help':
-    user_help()
-  else:
-    print('Oops! Please enter "YES" or "HELP".')
-    ask_if_player_is_ready()
+  def ask_if_player_is_ready():
+    q = input(
+      str('Type \'YES\' to begin or \'HELP\' if you\'d like more instructions: '))
+    if q == 'YES' or q == 'yes':
+      print('Starting new game...')
+      
+    elif q == 'HELP' or q == 'help':
+      user_help()
+    else:
+      print('Oops! Please enter "YES" or "HELP".')
+      ask_if_player_is_ready()
 
-def game_finished():
-  pass
 
-# === GAME SEQUENCE ====
-ask_if_player_is_ready()
-# word is sent to wordle.py class GameSession
-# tell user to make a guess
-print('Please type a five-letter word to get started.')
 
-# user inputs a guess
+  # === GAME SEQUENCE ====
+  ask_if_player_is_ready()
+  # word is sent to wordle.py class GameSession
+  # tell user to make a guess
+  print('Please type a five-letter word to get started.')
+
+  # user inputs a guess
   # guess word is sent to wordle.py class GameRound
-guess = input(str('Guess 1/6: '))
-wordle.GameRound.validate_word(guess)
-  # it valdiates that the word is an english word and is 5 characters length
+  player_guess = ''
 
+
+  def guess_word():
+    player_guess = input(str('Guess 1/6: '))
+    # check if word is 5 characters long
+    if len(player_guess) != 5:
+      print('Please enter a five-letter word!')
+      guess_word()
+    else:
+      # check if word is in the dictionary
+      is_in_dictionary = wordle.GameRound.check_dictionary(player_guess)
+      if not is_in_dictionary:
+        print(f'Could not find the word "{
+              player_guess}" in the dictionary. Try again.')
+        guess_word()
+      else:
+        print(f'"{player_guess}" is in the dictionary!')
+    
+    print('WILL THIS PRINT EVERYTIME I MAKE AN ERROR GUESS??')
+    # answer for above: this prints at the end of this function.. but = to the amount of errors
+
+
+  guess_word()
+
+
+  # wordle.GameRound.make_guess(player_guess)
+
+  # validate word guess with if statement
+
+  # it valdiates that the word is an english word and is 5 characters length
--- src/wordle.py
+++ src/wordle.py
@@ -1,36 +1,29 @@
 from PyDictionary import PyDictionary
-dictionary=PyDictionary()
-
-print(bool(dictionary.meaning("indentation")))
-print(bool(dictionary.meaning("aware")))
+dictionary = PyDictionary()
 
 
 class GameSession():
-  def __init__(self, wordle, session_guesses=[]):
-    self.wordle = wordle
-    print("GAMESESSION >>>>> wordle is : " + str(wordle))
+  def __init__(self, wordle_word, ):
+    self.wordle_word = wordle_word
+    print("GAMESESSION >>>>> wordle is : " + str(wordle_word))
 
   # METHODS
-  # 1. display user session guesses with colour indicators
-  #
+  # display user session guesses with colour indicators
 
 
 # a GameSession constitutes 6 game rounds where the player makes a guess each round
 class GameRound(GameSession):
-  def __init__(self, wordle):
-    self.wordle = wordle
+  # def __init__(self, wordle):
+  #   self.wordle = wordle
 
-# METHODS - what can players do in a game round
   # validate word_guess
-  def validate_word(word):
-    print(f'validating {word}...')
-    
+  def check_dictionary(word):
+    print(f'checking: {word}...')
+    is_in_dictionary = bool(dictionary.meaning(word))
+    print(f'checking: {is_in_dictionary}')
+    return is_in_dictionary
 
-
-  # 1. make a guess
+  # make a guess
   def make_guess(word_guess):
     print('The user has guessed the word ' + word_guess)
-
-
-
   # loop over guess word characters and check if it matches wordle_word characters
