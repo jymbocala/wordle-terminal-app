a16960cbb748f7432f1a7fffb81e81457f2b6724
jymbocala <jym.bocala@gmail.com>
2023-10-24 12:05:39 +1100

    add make_guess method

--- src/wordle.py
+++ src/wordle.py
@@ -5,10 +5,22 @@ dictionary = PyDictionary()
 class GameSession():
   def __init__(self, wordle_word):
     self.wordle_word = wordle_word
+    self.guessed_words = []  # Initialize an empty list to store guessed words.
+    self.attempts_left = 6 # Max number of attempts
+
+  # make a guess
+  def make_guess(self, word_guess):
+    print('The user has guessed the word ' + word_guess)
+
+    if word_guess == self.wordle_word:
+      print(f'You guessed the correct word: {self.wordle_word}')
+    else:
+      self.attempts_left -= 1
+      return "Incorrect guess. Try again."
+
 
   def is_game_over(self):
     False
-
 
 
 class GameRound():
@@ -27,20 +39,14 @@ class GameRound():
     if len(player_guess) != 5:
       print('Please enter a five-letter word!')
       return self.get_player_word()
-  
+
     # check if word is in the dictionary
     is_in_dictionary = bool(dictionary.meaning(player_guess))
     if not is_in_dictionary:
-      print(f'Could not find the word "{player_guess}" in the dictionary. Try again.')
+      print(f'Could not find the word "{
+            player_guess}" in the dictionary. Try again.')
       return self.get_player_guess()
     else:
       print(f'"{player_guess}" is in the dictionary!')
 
     return player_guess
-
-
-
-  # make a guess
-  def make_guess(word_guess):
-    print('The user has guessed the word ' + word_guess)
-  # loop over guess word characters and check if it matches wordle_word characters
