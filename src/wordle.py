dfa6fd3d35f0ce77ae34c9ebd230bb984776167e
jymbocala <jym.bocala@gmail.com>
2023-10-26 13:33:39 +1100

    add is_game_won method

--- src/wordle.py
+++ src/wordle.py
@@ -18,9 +18,8 @@ class GameSession():  # Manages the game state and logic
     import os
     os.system('clear' if os.name == 'posix' else 'cls')
 
+    # add player guess to guessed_word list
     self.guessed_words.append(word_guess)
-    print('Current guessed_words: ')
-    print(self.guessed_words)
 
     if word_guess == self.wordle_word:
       return f'Congratulations! You have correctly guessed the word: {
@@ -41,33 +40,31 @@ class GameSession():  # Manages the game state and logic
     if self.wordle_word in self.guessed_words:
       return True
     return False
-
+  
+  def is_game_won(self):
+    return self.wordle_word in self.guessed_words
 
 class GameRound():  # Handles displaying information and taking input from the user.
   def display_game_state(self, game):
-    # display previous guesses
-    # if game.guessed_words:
-    #   print('Previous guesses:')
-    #   for word in game.guessed_words:
-    #     print(word)
-
     if game.guessed_words:
       for i, guess in enumerate(game.guessed_words, start=1):
-        formatted_guess_text = ""
+        formatted_guess_text = []
 
         for wordle_char, guessed_char in zip(game.wordle_word, guess):
           if guessed_char == wordle_char:
-            formatted_guess_text += f"[on green1]{guessed_char}[/] "
+            formatted_guess_text.append(f"[bold green]{guessed_char}[/]")
           elif guessed_char in game.wordle_word:
-            formatted_guess_text += f"[on orange3]{guessed_char}[/] "
+            formatted_guess_text.append(f"[bold orange3]{guessed_char}[/]")
           else:
-            formatted_guess_text += f"[on grey62]{guessed_char}[/] "
+            formatted_guess_text.append(f"[bold grey62]{guessed_char}[/]")
+        # Join the colored characters with spaces
+        formatted_guess_text = " ".join(formatted_guess_text)
+        # Print the styled text with center alignment
+        console.print(formatted_guess_text, justify="center")
+      print(f"Guesses left: {game.attempts_left}")  # display attempts left
 
-        # Create a Padding object with the formatted text
-        formatted_guess_with_padding = Padding(formatted_guess_text, (0, 1), style="on blue", expand=False)
-
-        # Print the styled text with center alignment and padding
-        console.print(f"Guess {i}:", formatted_guess_with_padding, justify="center")
+      # TODO:
+      # fix: padded characters are not showing up with correct styles, and they are print in different lines
 
   def get_player_guess(self):
     player_guess = input(str('Enter word: ')).upper()
@@ -85,3 +82,14 @@ class GameRound():  # Handles displaying information and taking input from the u
       return self.get_player_guess()
     else:
       return player_guess
+
+  def display_outcome(self, is_win):
+      # Display all 6 guesses to the user
+    for i, guess in enumerate(self.guesses, start=1):
+      print(f"Guess {i}: {guess}")
+
+    # Check if it's a win or a loss
+    if is_win:
+      print("Congratulations! You've won!")
+    else:
+      print("Oh no! You've run out of guesses. The word was:", self.wordle_word)
