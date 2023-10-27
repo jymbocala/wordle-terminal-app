b2b6ad897c8beb38997ce58ece89dfb69acfdbe4
jymbocala <jym.bocala@gmail.com>
2023-10-28 00:55:16 +1100

    add complete scripts

--- wordle_script.sh
+++ wordle_script.sh
@@ -2,9 +2,13 @@
 if ! [[ -x "$(command -v python3)" ]]; 
 then
     echo "Python 3 is not installed. Please install Python 3 to play Wordle."
-    # Provide a link to install Python 3
     echo "You can download Python 3 from the official website:"
     echo "https://www.python.org/downloads/"
     exit 1
 fi
 
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+python3 src/main.py
+deactivate
